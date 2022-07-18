85b92777ecf3bdc1364663b3f0668254e76f5621
Neo <91592026+AlexRixten@users.noreply.github.com>
2022-07-18 16:01:48 +0300

    app init

---
+++ src/components/services/ServiceItem.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const ServiceItem = () => {
+  return (
+    <div>ServiceItem</div>
+  )
+}
---
+++ src/pages/Portfolios.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Portfolios = () => {
+  return (
+    <div>Portfolios</div>
+  )
+}
