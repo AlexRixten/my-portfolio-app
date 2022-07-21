8c50a874ce0519f717b4ade1b95fc1f88217bad5
Neo <91592026+AlexRixten@users.noreply.github.com>
2022-07-21 11:14:32 +0300

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
     extend: {
       colors:{
         orangeColor: '#FFB400',
-        grayColor: '#767676'
+        grayColor: '#767676',
+        mainGray: '#242526',
+      },
+      shadow:{
+        boxShadow: 'rgb(28 28 28 / 10%) 4px 0px 10px',
       }
     },
   },
