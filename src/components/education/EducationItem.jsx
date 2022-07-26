692537f9474a2c4beb20de9e8a76748d24958074
Neo <91592026+AlexRixten@users.noreply.github.com>
2022-07-26 22:28:03 +0300

    educationItem style

--- src/components/education/EducationItem.jsx
+++ src/components/education/EducationItem.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
 
+import style from '../../sass/components/Education.module.scss'
+
 export const EducationItem = (props) => {
   return (
-    <div className='flex -lg:flex-col mb-5 border-b-2 border-solid border-white last-of-type:border-none last-of-type:mb-0'>
-        <div className='flex-[3]'>
-            <h4 className='text-xl font-bold mb-4'>{props.title}</h4>
-            <p className='flex -2xl:flex-col items-center -2xl:items-start mb-5'>
+    <div className={style.educationItem}>
+        <div className={style.leftSide}>
+            <h4 className={style.title}>{props.title}</h4>
+            <p className={style.description}>
                 {props.position}: 
-                <span className='ml-2 -2xl:ml-0 bg-orangeColor p-2 text-xs w-fit'>{props.period}</span>
+                <span className={style.period}>{props.period}</span>
             </p>
         </div>
-        <div className='flex-[6]'>
-            <h4 className='text-xl font-bold mb-4'>{props.name}</h4>
-            <p className=''>
+        <div className={style.rightSide}>
+            <h4 className={style.title}>{props.name}</h4>
+            <p className={style.mainDescription}>
                 {props.description}
             </p>
         </div>
